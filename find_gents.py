132de4af497bd699b04a64a0726e4e332be71b2b
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-05-21 14:53:58 -0600

    Renamed function 'excel_sheet_ext_good()' to 'file_ext_good()' and made function more general so it can be applied to files with different file extensions

--- find_gents.py
+++ find_gents.py
@@ -23,8 +23,8 @@ def check_cmd_args():
     # Read the excel file genome annotation, which should be 
     # the first command line argument and check the file 
     # extension
-    if len(sys.argv) > 1:
-        if not excel_sheet_ext_good(sys.argv[1]):
+    if len(sys.argv) > 2:
+        if not file_ext_good(sys.argv[1], 'xls'):
             # Print error message
             err = "Error: First command line argument must be "
             err += "an excel sheet\n"
@@ -40,26 +40,29 @@ def check_cmd_args():
         print_usage()
 
 
-def excel_sheet_ext_good(filename):
+def file_ext_good(filename, ext):
     """
     This function just checks that the string variable 'filename'
-    passed into the function has a .xls extension. The string
+    passed into the function has the correct extension. The string
     variable 'filename' represents the filename of the excel
-    sheet genome annotation for the organism. This function
-    will return True if 'filename' has the .xls extenstion.
+    sheet genome annotation for the organism. The string 'ext' 
+    represent the file extension that the filename is expected to 
+    have. This function will return True if 'filename' has the 
+    extenstion equal to the string stored in the 'ext' parameter.
     Otherwise it will return False.
     
-    :param filename: Filename for the excel sheet.
+    :param filename: Filename for input file.
+    :param ext: The expected extension for 'filename'
     :return: True if filename has the .xls extension
     :return: False Otherwise
     """
-    excel_file_name = sys.argv[1]
+    file_name = sys.argv[1]
     # Find the position of the '.' char for determining the file
     # extension
-    pos = excel_file_name.rfind(".")
+    pos = file_name.rfind(".")
     # Get the extension substring in the excel_file_name by 
-    ext = excel_file_name[pos+1:]
-    if ext == 'xls':
+    file_ext = file_name[pos+1:]
+    if file_ext == ext:
         # File ext good, return True
         return True   
     # File ext not good, return False
