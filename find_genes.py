d1ffef075c5fe963b90b71b7eee86b70a3a29cdd
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-05-24 05:31:28 -0600

    Finished writing program to parse the RAST excel spreadsheet for protein EC Numbers and marking which enzymes are present in the species from the KEGG biochemical pathways.

--- find_genes.py
+++ find_genes.py
@@ -1,54 +1,28 @@
 # Import Modules
 import sys
+import os
 import csv
 import codecs
 import pandas as pd
+import urllib.request
+from PIL import Image, ImageDraw as D
 
 
 
 ########################## Functions ############################
 
-def check_cmd_args():
+def parse_cmd_args():
     """
-    This function checks the command line arguments into the 
-    program. To do this, this function will call other
-    functions like 'excel_sheet_ext_good' and 'print_usage'. If
-    the command line arguments are good, then this function
-    will return True. Otherwise, it will print an error message
-    and return False.
+    This function parses the command line arguments
     
-    :return: True if the command line arguments are correct.
-    :return: False otherwise
+    :return: A dictionary of the arguments
     """
-    # Read the excel file genome annotation, which should be 
-    # the first command line argument and check the file 
-    # extension
-    if len(sys.argv) > 2:
-        if not file_ext_good(sys.argv[1], 'xls'):
-            # Print error message
-            err = "Error: First command line argument must be "
-            err += "an excel sheet\n"
-            print(err)
-            # Print usage statement
-            print_usage()
-            return False
-        # 2nd command line argument is expected to be a txt file
-        elif not file_ext_good(sys.argv[2], 'txt'):
-            # Print error message
-            err = "Error: Second command line argument must be "
-            err += "an txt file\n"
-            print(err)
-            # Print usage statement
-            print_usage()
-            return False
-        return True
-    # Print error message
-    err = "Error: Invalid number of command line " 
-    err += "arguements\n"
-    print(err)
-    # Print usage statement
-    print_usage()
-    return False
+    args = {}
+    for arg in sys.argv:
+        pos = arg.find("=")
+        if pos > -1:
+            args[arg[:pos]] = arg[pos+1:]
+    return args
 
 
 def file_ext_good(filename, ext):
@@ -79,6 +53,59 @@ def file_ext_good(filename, ext):
     return False
     
     
+def getCoords(pos):
+    """
+    Function to get the coordinates of a protein from the pathway .htm file
+    
+    :param pos: The start position to find the nearest coordinates before 
+                that point in the file
+    :return: The start coordinates as a tuple
+    :return: The end coordinates as a tuple
+    """
+    coordPos = source_code.rfind("coords", 0, pos)
+    coordsStart = source_code.find('"', coordPos) + 1
+    coordsEnd = source_code.find('"', coordsStart)
+    theCoords = source_code[coordsStart:coordsEnd]
+    sep = theCoords.find(",")
+    sep2 = theCoords.find(",", sep+1)
+    sep3 = theCoords.find(",", sep2+1)
+    startCoords = (int(theCoords[0:sep]), \
+                   int(theCoords[sep+1:sep2]))
+    endCoords   = (int(theCoords[sep2+1:sep3]), \
+                   int(theCoords[sep3+1:]))
+    return startCoords, endCoords
+    
+    
+def getPathwayFiles(baseName):
+    """
+    Function to return the .htm and pathway image filepaths associated
+    with a pathway. 
+    
+    :param baseName: The basename for the pathway
+    :return: Filepath for the htm file associated with the pathway
+    :return: Filepath for the pathway's map image
+    """
+    htmFilepath = baseName + ".htm"
+    pathwayFiles = baseName + "_files"
+    if not os.path.isfile(htmFilepath):
+        print(htmFilepath + " does not exist\nQuitting...")
+        exit(1)
+    # Get pathway map filepath
+    if not os.path.isdir(pathwayFiles):
+        print(pathwayFiles + " does not exist")
+        print("Quitting...")
+        exit(2)
+    for f2 in os.listdir(pathwayFiles):
+        if f2[:3] == "map" and not (f2.rfind(".") > -1 \
+        and f2.rfind("_") > -1 \
+        and f2[f2.rfind("_"):f2.rfind(".")] == "_annotated"):
+            pathwayMapIm = pathwayFiles + "\\" + f2
+    if not os.path.isfile(pathwayMapIm):
+        print(pathwayMapIm + " does not exist\nQuitting...")
+        exit(3)
+    return htmFilepath, pathwayMapIm
+    
+    
 def print_usage():
     """
     Function that prints the usage statement for the program.
@@ -95,17 +122,78 @@ def print_usage():
 
 ########################### Main ################################
 
-# Check the command line arguements
-if not check_cmd_args():
-    exit()
+# Parse command line arguments
+args = parse_cmd_args()
+
 # Read the xls file
-df = pd.read_excel(sys.argv[1])
+df = pd.read_excel(args["RAST_Spreadsheet"])
 # Store EC numbers in a python set
-proteins = set(())  
+ecNumbers = set(())  
 for protein in df.function:
     pos = protein.find("(EC ")
     if pos > 0:
-        proteins.add(protein[pos+4:protein.find(")", pos)].strip())
+        ecNumbers.add(protein[pos+4:protein.find(")", pos)].strip())
+# Read and store protein abbreviations
+proteinAbbrevs = set(())
+if "protein_abbrev" in args:
+    with open(args["protein_abbrev"],'r') as file:  
+        # reading each line    
+        for line in file:
+            # reading each word        
+            for abbrev in line.split(): 
+                # displaying the words           
+                proteinAbbrevs.add(abbrev.strip())
+       
+pathwaysPath = args["pathways"]
+while pathwaysPath[len(pathwaysPath)-1] == "\\":
+    pathwaysPath = pathwaysPath[:len(pathwaysPath)-1]
+    
+pathwayNames = set(())
+for f in os.listdir(pathwaysPath):
+    if not(len(f) > 11 and f[len(f)-12:] == "_updated.htm") \
+    and file_ext_good(f, 'htm'):
+        baseName = f[:f.rfind(".")]
+        pathwayNames.add(baseName)
         
-#for protein in proteins:
-    #print(protein)
+for baseName in pathwayNames:
+    # Get htm file
+    htmFilepath, pathwayMapIm = getPathwayFiles(pathwaysPath + "\\" \
+                              + baseName)
+        
+    i=Image.open(pathwayMapIm)
+    draw=D.Draw(i)
+    HtmlFile = open(htmFilepath, 'r', encoding='utf-8')
+    source_code = HtmlFile.read()
+    # Draw Green boxes around EC Numbers
+    for ec in ecNumbers:
+        pos = 0
+        while pos > -1:
+            pos = source_code.find("+"+ec+'+', pos+1)
+            if pos > -1:
+                startCoords, endCoords = getCoords(pos)
+                draw.rectangle([startCoords,endCoords],outline="green")
+    # Draw Green boxes around Protein Abbreviations
+    for abbrev in proteinAbbrevs:
+        pos = 0
+        while pos > -1:
+            pos = source_code.find("("+abbrev+')', pos+1)
+            if pos > -1:
+                print(abbrev)
+                startCoords, endCoords = getCoords(pos)
+                draw.rectangle([startCoords,endCoords],outline="green")
+    newPathwayMapIm = pathwayMapIm[:pathwayMapIm.rfind(".")] + "_annotated" \
+                    + pathwayMapIm[pathwayMapIm.rfind("."):]
+    imPath = source_code.find('<img src="KEGG%20PATHWAY')
+    imPathStart = source_code.find('"', imPath)
+    imPathEnd = source_code.find('"', imPathStart+1)
+    source_code = source_code[:imPathStart+1] + newPathwayMapIm \
+                + source_code[imPathEnd:]
+    newHtmFilepath = htmFilepath[:htmFilepath.rfind(".")] + "_updated" \
+                   + htmFilepath[htmFilepath.rfind("."):] 
+    i.save(newPathwayMapIm)
+    updatedHtmlFile = open(newHtmFilepath, "w", encoding='utf-8')
+    updatedHtmlFile.write(source_code)
+    #i.show()
+    HtmlFile.close()
+    updatedHtmlFile.close()
+    i.close()
