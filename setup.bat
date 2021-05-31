207c2424360a234c258ee0df49016f78760c8480
Kovarik, Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-05-31 17:27:01 -0600

    Wrote scrpt to install dependencies

---
+++ setup.bat
@@ -0,0 +1,12 @@
+:: This batch file installs the dependencies for the project
+ECHO OFF
+:: Update PIP
+call py -m pip install --upgrade pip
+:: Install Pandas
+call py -m pip install pandas
+:: Install Pillow
+call py -m pip install Pillow
+:: Install openpyxl
+call py -m pip install openpyxl
+:: Install PySimpleGUI
+call py -m pip install PySimpleGUI
